e114c282ddad3b941da6e3e5989eb23bc1ca3ba4
Osher Solimany <solimani.osher@gmail.com>
2023-11-21 20:11:35 +0200

    Add basic routes and controllers for all routes

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const app = express();
 
 const tasksRouter = require('./routes/tasks');
 
-const port = 3000;
+const app = express();
 
+app.use(express.json());
 app.use('/api/v1/tasks', tasksRouter);
 
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,6 +2,26 @@ const getAllTasks = (req, res) => {
   res.send('getAllTasks');
 };
 
+const createTask = (req, res) => {
+  res.send('createTask');
+};
+
+const getSingleTask = (req, res) => {
+  res.send('getSingleTask');
+};
+
+const updateTask = (req, res) => {
+  res.send('updateTask');
+};
+
+const deleteTask = (req, res) => {
+  res.send('deleteTask');
+};
+
 module.exports = {
   getAllTasks,
+  createTask,
+  getSingleTask,
+  updateTask,
+  deleteTask,
 };
