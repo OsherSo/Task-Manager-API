76b810b77828e516cf0ba6f7b425fa87e78af9cb
Osher Solimany <solimani.osher@gmail.com>
2023-11-21 21:13:18 +0200

    Update mongoose and finish connection to DB

--- db/connect.js
+++ db/connect.js
@@ -1,2 +1,13 @@
+const mongoose = require('mongoose');
+
 const connectionString =
-  'mongodb+srv://Kazoon:<password>@kazoon.bsoisgk.mongodb.net/?retryWrites=true&w=majority';
+  'mongodb+srv://Kazoon:Beitar1936@kazoon.bsoisgk.mongodb.net/TaskManager?retryWrites=true&w=majority';
+
+mongoose
+  .connect(connectionString)
+  .then(() => {
+    console.log('Connected to DB...');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
