a1a63c05503943d41c4e8bc7864d91d9823f9bfb
Osher Solimany <solimani.osher@gmail.com>
2023-11-22 12:16:11 +0200

    Refactor DB Connect

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-require('./db/connect');
 const express = require('express');
 
+const connectDB = require('./db/connect');
 const tasksRouter = require('./routes/tasks');
 
 const app = express();
@@ -9,6 +9,15 @@ app.use(express.json());
 app.use('/api/v1/tasks', tasksRouter);
 
 const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
--- db/connect.js
+++ db/connect.js
@@ -3,11 +3,8 @@ const mongoose = require('mongoose');
 const connectionString =
   'mongodb+srv://Kazoon:Beitar1936@kazoon.bsoisgk.mongodb.net/TaskManager?retryWrites=true&w=majority';
 
-mongoose
-  .connect(connectionString)
-  .then(() => {
-    console.log('Connected to DB...');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const connectDB = (url) => {
+  return mongoose.connect(connectionString);
+};
+
+module.exports = connectDB;
